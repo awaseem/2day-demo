8d092f5068364ea1798747207168c79448465977
Ali Waseem <github@aliwaseem.com>
2023-04-15 22:15:38 -0600

    remove logging

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -29,8 +29,6 @@ export async function getServerSideProps() {
 }
 
 export default function Home({ voxData, vergeData, redditData }: HomeProps) {
-  console.log(voxData);
-
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-16">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
